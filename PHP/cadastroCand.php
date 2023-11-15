7a17c5433e1f4e35a1977b69be955a61edba776c
LucasCoutinhodSousa <lucascoutinho-dsousa@hotmail.com>
2023-11-14 21:03:02 -0300

    BackEnd

--- PHP/cadastroCand.php
+++ PHP/cadastroCand.php
@@ -2,6 +2,9 @@
 include('conexao.php')
 
 try{
-    if(isset($_GET[]))
+    if(isset($_GET['nome'], $_GET['email'], $_GET['cpf'], $_GET['senha'], $_GET['telefone'], $_GET['sexo'], $_GET['cep'], $_GET['municipio'], $_GET['pais'], $_GET['bairro'], $_GET['estado'], $_GET['estado'], $_GET['rua'], $_GET['empresa'], $_GET['profInicio'], $_GET['cargo'], $_GET['profFinal'], $_GET['profDescricao'], $_GET['formacao'], $_GET['instituicao'], $_GET['status'], $_GET['acadInicio'], $_GET['curso'], $_GET['acadFinal'], $_GET['acadDescricao'])){
+        $nome = $_GET['nome'];
+        $email = $_GET['email'];
+    }
 }
 ?>
