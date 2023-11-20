3d3831d0f0ee2dcde1aabf2a62a29a12c89b5457
lorenzzosoaresnogueira <lorezzosoaresnogueira@outlook.com>
2023-11-20 17:06:26 -0300

    alteracao

--- PHP/gestaodecontratacoes.php
+++ PHP/gestaodecontratacoes.php
@@ -4,20 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>gestaodecontratacoes</title>
-    <link rel="stylesheet" href="../CCS/gestaodecontratacoes.css">
+    <link rel="stylesheet" href="../CSS/gestaodecontratacoes.css">
 </head>
 <body>
+        
+
     <div class="container">
-        
-           
-        
-        <div class="fundobranco"><h1>Gestão de contratações</h1></div>
-       
-        <div class="botoes">
-            <button class="btn1" type="submit" id="adicionar-vaga">Adicionar vaga</button>
-            <button class="btn1" type="submit" id="caonsultar-candidatos">Consultar candidatos</button>
-            <button class="btn1" type="submit" id="Vagas-abertas">Vagas abertas</button> 
+        <div class="fundobranco"> <h1> Gestão de contratações</h1>
+
+         <button class="btn1" type="submit" id="adicionar-vaga"> Adicionar vaga</button>
+         <button class="btn2" type="submit" id="consultar-candidato">Consultar candidato</button>
+         <button class="btn3" type="submit" id="vagas-abertas">Vagas abertas</button>
+
+
         </div>
+
+        <div class="esquerda">
+            <h1>Senai Vitória</h1>
+
+        </div>
+
+
+
     </div>
 </body>
 </html>
