5a404740039d6d035d47f5e7f12c86bf794596b8
LucasCoutinhodSousa <lucascoutinho-dsousa@hotmail.com>
2023-11-25 16:56:01 -0300

    inicio chat

--- api/sistemaChat.php
+++ api/sistemaChat.php
@@ -1,3 +1,17 @@
-<?php?>
+<?php
 
 namespace Api\Websocket;
+
+use Ratchet\Websocket\MessageComposerInterface;
+
+class sistemaChat implements MessageComposerInterface{
+    protected $cliente;
+
+    public function _construct()
+    {   
+        //Iniciar o objetos que  deve amazenar os clientes conectados
+        $this->cliente = new  \SplObjectStorenge;
+    }
+
+    
+}
