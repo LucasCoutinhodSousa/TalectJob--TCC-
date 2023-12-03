e33e377785aac5663ef6a472750d5e43604dd78e
LucasCoutinhodSousa <lucascoutinho-dsousa@hotmail.com>
2023-12-02 23:53:11 -0300

    pronto

--- PHP/portalDVagas.php
+++ PHP/portalDVagas.php
@@ -1,6 +1,33 @@
 <?php
     include('conexao.php');
-    $query = $dbh->prepare('SELECT * FROM cadasVagas');
+
+    // Defina o número de itens por página
+    $itensPorPagina = 3;
+
+    // Obtenha o número total de vagas
+    $totalVagas = $dbh->query('SELECT COUNT(*) FROM cadasVagas')->fetchColumn();
+
+    // Calcule o número total de páginas
+    $totalPaginas = ceil($totalVagas / $itensPorPagina);
+
+    // Obtenha o número da página atual a partir do parâmetro de consulta
+    $paginaAtual = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+
+    $localFiltro = isset($_GET['local']) ? $_GET['local'] : '';
+
+    // Calcule o offset para a consulta
+    $offset = ($paginaAtual - 1) * $itensPorPagina;
+
+    if ($localFiltro != '') {
+        $query = $dbh->prepare('SELECT * FROM cadasVagas WHERE localVaga = :localVaga LIMIT :offset, :itensPorPagina');
+        $query->bindParam(':localVaga', $localFiltro, PDO::PARAM_STR);
+    } else {
+        $query = $dbh->prepare('SELECT * FROM cadasVagas LIMIT :offset, :itensPorPagina');
+    }
+
+    // Consulta para obter apenas os itens da página atual
+    $query->bindParam(':offset', $offset, PDO::PARAM_INT);
+    $query->bindParam(':itensPorPagina', $itensPorPagina, PDO::PARAM_INT);
     $query->execute();
 
     $vagas = $query->fetchAll(PDO::FETCH_ASSOC);
@@ -32,16 +59,17 @@
         <div class="filto-to">
             <div class="filtro">
                 <h1>Filtro</h1>
-                <select name="" id="" aria-placeholder="Filtro">
-                    <?php
-                        foreach($vagas as $local){
-                            echo '<option>'.$local['localVaga'].'</option>';
+                <form method="get" action="pagina.php">
+                    <select name="local" id="local" onchange="this.form.submit()">
+                        <option value="" <?php echo ($localFiltro == '') ? 'selected' : ''; ?>>Todos os Locais</option>
+                        <?php
+                        foreach ($vagas as $local) {
+                            echo '<option value="' . $local['localVaga'] . '" ' . (($localFiltro == $local['localVaga']) ? 'selected' : '') . '>' . $local['localVaga'] . '</option>';
                         }
-                    ?>
-                    <option value="">Local</option>
-                    <option value="">Local</option>
-                    <option value="">Local</option>
-                </select>
+                        ?>
+                    </select>
+                </form>
+
                 <select name="" id="">
                     <option value="">Área profissional</option>
                     <option value="">Área profissional</option>
@@ -56,34 +84,23 @@
         </div>
         <div class="vagas-to">
 
-        <?php
-        foreach($vagas as $vaga){
-            echo '<div class="vagas">';
-            echo  '<a href="../PHP/vaga.php?id='.$vaga['id'].'">';
-            echo  '<h1>'.$vaga['cargo'].'</h1>';
-            echo  '<h2>'.$vaga['localVaga'].'</h2>';
-            echo    '<p>'.$vaga['descricaoVaga'].'</p>';
-            '</a>';
-           echo '</div>';
-            }
-        ?>
-            <!--<div class="vagas">
-                <h1>Analista de Sistema</h1>
-                <h2>São Paulo</h2>
-                <p>Domínio de configurações, recomendações e boas práticas de segurança para sistemas em nuvem; Experiência com tecnologia
-                Cloud (IaaS, PaaS, DBaaS, SaaS); Conhecimento avançado nos sistemas operacionais Windows e Linux; Domínio de requisitos
-                não funcionais (ex: desempenho, disponibilidade, segurança, interoperabilidade);
-                </p>
+            <?php
+            foreach($vagas as $vaga){
+                echo '<div class="vagas">';
+                    echo  '<a href="../PHP/vaga.php?id='.$vaga['id'].'">';
+                    echo  '<h1>'.$vaga['cargo'].'</h1>';
+                    echo  '<h2>'.$vaga['localVaga'].'</h2>';
+                    echo  '<p>'.$vaga['descricaoVaga'].'</p>';
+                    echo  '</a>';
+                echo '</div>';
+                }
+            ?>
+            <div class="pagination">
+                <?php for ($i = 1; $i <= $totalPaginas; $i++) : ?>
+                    <a href="?pagina=<?= $i ?>"><?= $i ?></a>
+                <?php endfor; ?>
             </div>
 
-            <div class="vagas">
-                <h1>Analista de Sistema</h1>
-                <h2>São Paulo</h2>
-                <p>Domínio de configurações, recomendações e boas práticas de segurança para sistemas em nuvem; Experiência com tecnologia
-                Cloud (IaaS, PaaS, DBaaS, SaaS); Conhecimento avançado nos sistemas operacionais Windows e Linux; Domínio de requisitos
-                não funcionais (ex: desempenho, disponibilidade, segurança, interoperabilidade);
-                </p>
-            </div>-->
         </div>
     </div>
 </body>
